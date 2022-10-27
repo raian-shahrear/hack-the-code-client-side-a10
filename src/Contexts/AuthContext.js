c1b9998c7e6e9bf69243105cdaca533c311fc534
raian-shahrear <raianshahrearbd@gmail.com>
2022-10-27 17:11:45 +0600

    SignOut, CurrentlyLoggedInUser and Loading have been added in AuthContext

--- src/Contexts/AuthContext.js
+++ src/Contexts/AuthContext.js
@@ -1,6 +1,8 @@
 import React, { createContext } from 'react';
-import { createUserWithEmailAndPassword, FacebookAuthProvider, getAuth, GithubAuthProvider, GoogleAuthProvider, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, updateProfile } from "firebase/auth";
+import { createUserWithEmailAndPassword, FacebookAuthProvider, getAuth, GithubAuthProvider, GoogleAuthProvider, onAuthStateChanged, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from '../Firebase/firebase.config'
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 export const UserContext = createContext();
 const auth = getAuth(app);
@@ -9,24 +11,31 @@ const AuthContext = ({children}) => {
   const googleProvider = new GoogleAuthProvider();
   const githubProvider = new GithubAuthProvider();
   const facebookProvider = new FacebookAuthProvider();
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(false);
   
+
   // Create a user account
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   }
 
   // Login user
   const loginUser = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   }
 
   // Reset password
   const resetPassword = (email) => {
+    setLoading(true);
     return sendPasswordResetEmail(auth, email);
   }
 
   // Update user information like Email & Password
   const updateUserInfo = (name, imageURL) => {
+    setLoading(true);
     return updateProfile(auth.currentUser, {
       displayName: name,
       photoURL: imageURL
@@ -35,11 +44,13 @@ const AuthContext = ({children}) => {
 
   // Verify email
   const verifyEmail = () => {
+    setLoading(true);
     return sendEmailVerification(auth.currentUser);
   }
 
   // Google login/signUp
   const googleUser = () => {
+    setLoading(true);
     return signInWithPopup(auth, googleProvider);
   }
   // GitHub login/signUp
@@ -48,11 +59,29 @@ const AuthContext = ({children}) => {
   }
   // Facebook login/signUp
   const facebookUser = () => {
+    setLoading(true);
     return signInWithPopup(auth, facebookProvider);
   }
 
+  // Get currently logged in user
+  useEffect( () => {
+    const unsubscribe = onAuthStateChanged(auth, currentUser => {
+      setUser(currentUser);
+      setLoading(false);
+    })
+    return () => {
+      unsubscribe();
+    }
+  }, [])
 
-  const authInfo = {createUser,loginUser, resetPassword, updateUserInfo, verifyEmail, googleUser, githubUser, facebookUser};
+  // Sign out user
+  const signOutUser = () => {
+    setLoading(true);
+    return signOut(auth);
+  }
+
+
+  const authInfo = {user, loading, createUser,loginUser, resetPassword, updateUserInfo, verifyEmail, googleUser, githubUser, facebookUser, signOutUser};
 
   return (
     <div>
